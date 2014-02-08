38232ea7fd479ba2546f9f56f2021d746cafaf86
Peter Whitesell <peter@thinkpad.(none)>
2014-02-08 03:28:43 -0600

    moved filters to widget from example

--- js/ph-widget.js
+++ js/ph-widget.js
@@ -1,5 +1,29 @@
 var phWidget = angular.module('phWidget', ['jQueryUISlider', 'scrollingSlideShow', 'ngTouch']);
 
+phWidget.filter('costs', function(){
+	return function(items, lowerBound, upperBound){
+		return items.filter(function(item){
+			return item.price >= lowerBound && item.price <= upperBound;
+		});
+	};
+});
+
+phWidget.filter('colored', function(){
+	return function(items, colors){
+		return items.filter(function(item){
+			return colors.hasOwnProperty(item.color) && colors[item.color];
+		});
+	};
+});
+
+phWidget.filter('sized', function(){
+	return function(items, lowerBound, upperBound){
+		return items.filter(function(item){
+			return item.size >= lowerBound && item.size <= upperBound;
+		});
+	};
+});
+
 phWidget.directive('phWidgetContainer', function(){
 	return {
 		restrict: 'A',
