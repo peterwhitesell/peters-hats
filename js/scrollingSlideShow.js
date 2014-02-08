2d97f799e1d4648909e0206ea2af1491a8ea7390
Peter Whitesell <peter@thinkpad.(none)>
2014-02-08 04:14:14 -0600

    testing swipe in slide carousel

--- js/scrollingSlideShow.js
+++ js/scrollingSlideShow.js
@@ -1,39 +1,57 @@
 /**
  * simple scrolling slideshow courtesy of http://stackoverflow.com/questions/16050564/horizontal-scroll-on-mousemove-wide-div-in-smaller-div-with-overflowhidden-c#answer-16061097
  */
-var scrollingSlideShow = angular.module('scrollingSlideShow', []);
+var scrollingSlideShow = angular.module('scrollingSlideShow', ['ngTouch']);
 
-scrollingSlideShow.directive('phSlideShow', function(){
+scrollingSlideShow.directive('phSlideShow', ['$swipe', function($swipe){
 	return {
 		restrict: 'A',
 		link: function(scope, elm, attrs){
 			var interval = null;
+			var bl = $(elm),
+				th = $('.thumbs', bl),
+				blW, blSW, wDiff, mPadd, damp, mX, mX2, posX, mmAA, mmAAr;
+			
 			var rebuild = function(){
 				clearInterval(interval);
-				var bl    = $(elm);
-				var th    = $('.thumbs', bl);
 				th.css({marginLeft: 0});
-				var	blW   = bl.outerWidth(),
-					blSW  = bl[0].scrollWidth,
-					wDiff = (blSW/blW) - 1,  // widths difference ratio
-					mPadd = 60,  // Mousemove Padding
-					damp  = 20,  // Mousemove response softness
-					mX    = 0,   // Real mouse position
-					mX2   = 0,   // Modified mouse position
-					posX  = 0,
-					mmAA  = blW-(mPadd*2), // The mousemove available area
-					mmAAr  = (blW/mmAA);    // get available mousemove fidderence ratio
-
-				bl.mousemove(function(e) {
-			        mX = e.pageX - this.offsetLeft;
-			        mX2 = Math.min( Math.max(0, mX-mPadd), mmAA ) * mmAAr;
-				});
+				blW = bl.outerWidth();
+				blSW = bl[0].scrollWidth;
+				wDiff = (blSW/blW) - 1;  // widths difference ratio
+				mPadd = 60;  // Mousemove Padding
+				damp = 20;  // Mousemove response softness
+				mX = 0;   // Real mouse position
+				mX2 = 0;   // Modified mouse position
+				posX = 0;
+				mmAA = blW-(mPadd*2); // The mousemove available area
+				mmAAr = (blW/mmAA);    // get available mousemove fidderence ratio
 
 				interval = setInterval(function(){
 					posX += (mX2 - posX) / damp; // zeno's paradox equation "catching delay"
 					th.css({marginLeft: -posX*(wDiff) });
 				}, 10);
 			};
+			bl.mousemove(function(e) {
+		        mX = e.pageX - this.offsetLeft;
+		        mX2 = Math.min( Math.max(0, mX-mPadd), mmAA ) * mmAAr;
+			});
+
+			var touchXStart = null;
+			$swipe.bind(elm, {
+				'start': function(coords) {
+					touchXStarth = coords.x;
+				},
+				'move': function(coords) {
+					var delta = coords.x - pointX;
+					posX += delta;
+				},
+				'end': function(coords) {
+					// ...
+				},
+				'cancel': function(coords) {
+					// ...
+				}
+			});
 			$(document).ready(rebuild);
 			$(window).resize(rebuild);
 			scope.$watchCollection('hats | costs:priceRange.low:priceRange.high | sized:sizeRange.low:sizeRange.high | colored:colors', function(a, b){
@@ -41,4 +59,4 @@ scrollingSlideShow.directive('phSlideShow', function(){
 			});
 		}
 	};
-});
+}]);
